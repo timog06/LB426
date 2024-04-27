b18dc3a0eb12224c48184048bf5abe816839546f
Timog <110891995+timog06@users.noreply.github.com>
2024-04-27 19:49:14 +0200

    US-1.1-Full

--- GrandCasinoLB/GrandCasinoLB/CasinoMenu.cs
+++ GrandCasinoLB/GrandCasinoLB/CasinoMenu.cs
@@ -2,6 +2,15 @@
 {
     public class CasinoMenu
     {
-        
+        public void Run()
+        {
+            Console.Clear();
+            Console.Title = "Grand Casino LB";
+            Console.WriteLine("Welcome to Grand Casino LB!");
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Bank");
+            Console.WriteLine("3. Quit");
+            Console.ReadLine();
+        }
     }
 }
