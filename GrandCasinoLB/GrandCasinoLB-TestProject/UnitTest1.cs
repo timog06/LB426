c85de9cffa2e599467ae248d385a7d3a7ae2a2a4
Timog <110891995+timog06@users.noreply.github.com>
2024-04-27 19:10:42 +0200

    CasinoMenu

---
+++ GrandCasinoLB/GrandCasinoLB-TestProject/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GrandCasinoLB_TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GrandCasinoLB/GrandCasinoLB/CasinoMenu.cs
@@ -0,0 +1,70 @@
+﻿class CasinoMenu
+{
+    public void Run()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            Console.Title = "Grand Casino LB";
+            Console.WriteLine("Welcome to Grand Casino LB!");
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Bank");
+            Console.WriteLine("3. Quit");
+            Console.Write("Choose: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    PlayGame();
+                    break;
+                case "2":
+                    Bank();
+                    break;
+                case "3":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+
+    private void PlayGame()
+    {
+        Console.Clear();
+        Console.WriteLine("Roulette (1) / Slot Machine (2)");
+        Console.Write("Choose: ");
+        string gameChoice = Console.ReadLine();
+
+        switch (gameChoice)
+        {
+            case "1":
+                Console.WriteLine("You chose Roulette!");
+                break;
+            case "2":
+                Console.WriteLine("You chose Slot Machine!");
+                break;
+            default:
+                Console.WriteLine("Invalid choice, please try again.");
+                break;
+        }
+        Console.ReadKey();
+    }
+
+    private void Bank()
+    {
+        Console.Clear();
+        Console.WriteLine("How much money do you want to turn into chips?");
+        Console.Write("Enter amount: ");
+        string amount = Console.ReadLine();
+
+        Console.WriteLine($"You turned {amount} into chips.");
+        Console.ReadKey();
+    }
+}
