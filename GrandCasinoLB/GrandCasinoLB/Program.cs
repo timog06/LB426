2c06317677c901e262e6a378db5ed59dc282aaee
sprudello <110892985+sprudello@users.noreply.github.com>
2024-04-27 13:49:59 +0200

    Update Program.cs

--- GrandCasinoLB/GrandCasinoLB/Program.cs
+++ GrandCasinoLB/GrandCasinoLB/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GrandCasinoLB
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
