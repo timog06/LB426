0f5000a2e0d316db646a3b113d90c71098c5c445
Timog <110891995+timog06@users.noreply.github.com>
2024-04-28 20:51:28 +0200

    ObserverPattern-P1
    Observer Klasse wurde erstellt und ist bereit implementiert zu werden.

---
+++ GrandCasinoLB/GrandCasinoLB/ChipsObservable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandCasinoLB
+{
+    using System.Collections.Generic;
+
+    public class ChipsObservable
+    {
+        private List<IChipsObserver> _observers = new List<IChipsObserver>();
+        private int _chips;
+
+        public int Chips
+        {
+            get { return _chips; }
+            set
+            {
+                if (_chips != value)
+                {
+                    _chips = value;
+                    NotifyObservers();
+                }
+            }
+        }
+
+        public void AddObserver(IChipsObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void RemoveObserver(IChipsObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        private void NotifyObservers()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.ChipsUpdated(_chips);
+            }
+        }
+    }
+}
